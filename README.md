9d4f3e891463625b866ed39697993a2db788e09a
ehab-ahma <ehab24495@gmail.com>
2021-12-13 22:46:30 +0200

    feat:added solo and ranked component

--- README.md
+++ README.md
@@ -59,12 +59,7 @@
 
 [GitHub Projects - League Advisor Web App](https://github.com/orgs/League-Advisor/projects/2)
 
-- yarn
-- next 
-- react
-- javascript
-- tailwind
-
+- axios
 ---
 
 ## Getting Started
@@ -77,11 +72,22 @@
 
 v0.1: _feat: started project and created project repo_ - 08 Dec 2021
 
-v0.1.1 _docs: added requirements, domain model and db schema_ - 09 Dec 2021
+v0.1.1 _docs: added requirements, domain model and db schema_ - 08 Dec 2021
 
-v0.2.0: _feat: added next app_ - 12 Dec 2021 
+v0.1.2 _docs: edited domain model and db schema_ - 09 Dec 2021
+
+v0.2.1 _feat: created login component and added icons assets_ - 12 Dec 2021
+
+v0.2.2 _fix: added to login component_ - 12 Dec 2021
+
+
+v0.2.4 _feat:added webm background to login page and included assets_ - 13 Dec 2021
 
 v0.2.3: _feat: added sign up componenet_ - 12 Dec 2021 
+
+v0.2.4 _feat:added image bg to login page_ - 13 Dec 2021
+
+v0.2.6 _feat:added solo and ranked component_ - 13 Dec 2021
 
 ---
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
-import Signup from '../component/Signup'
-import SoloItem from '../component/SoloItem'
+import Login from '../components/Login'
+import Signup from '../components/Signup'
+import SoloItem from '../components/SoloItem'
 
 export default function Home() {
 	function handleSignUpForm(e){
 		e.preventDefault()
 		const email=e.target.email.value
-		const gameName=e.target.gameName.value
+		const summonerName=e.target.summonerName.value
 		const userName=e.target.userName.value
 		
 		const password1=e.target.password1.value
@@ -15,23 +16,30 @@ export default function Home() {
 			window.alert("Passwords do not match!")
 			
 		 }
-		console.log(email,gameName,userName,password1,password2);
+		console.log(email,summonerName,userName,password1,password2);
 	}
 	
+
+
   return (
-    <div className="items-center justify-center w-full min-h-screen py-2">
+    <div className="flex flex-col justify-center min-h-screen p-3">
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main>
-        <Signup handleSignUpForm={handleSignUpForm}/>
+
+      <main className="flex justify-center">
+        {/* <Signup handleSignUpForm={handleSignUpForm}/> */}
 		<SoloItem />
+		{/* <Login /> */}
+
       </main>
 
       <footer>
         
+
+
       </footer>
     </div>
   )
