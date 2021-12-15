0c6a63ab148d67743757b8f426420f46d6af6e17
Moha-AlHanbali <moha.alhanbali@gmail.com>
2021-12-15 19:02:34 +0200

    feat: modified dashboard styles

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -5,42 +5,45 @@ import Link from 'next/link'
 export default function Dashboard() {
 
     return (
-        <>
-            <div className="top-0 flex ">
+        <div className='flex h-screen bg-black'>
+
+            <video playsInline autoPlay loop muted className="flex pl-56 z-2 h-100" src="/Leona.webm" />
+
+
+            <div className="absolute left-0 z-0 flex w-full h-screen" >
+
                 <Navbar />
+                <div className="grid grid-cols-2 grid-rows-2 pt-16 z-1 gap-x-80">
 
-
-                <video playsInline autoPlay loop muted className="absolute top-0 right-0 w-10/12 h-screen " layout="fill" objectFit="cover" src="/Leona.webm" />
-
-                <div className="z-0 grid w-10/12 grid-cols-2 grid-rows-2 pl-4 h-top-0 right-2">
-
-                    <div className="flex justify-center w-8/12 h-48 mt-24 rounded-lg shadow-2xl ml-44 bg-white/50 z-1 ">
+                    <div className="z-0 flex justify-center w-full h-48 mt-24 rounded-lg shadow-2xl ml-44 bg-white/50 text-black/50 hover:bg-white/75 hover:text-black/75 active:bg-white/25 active:text-black ">
                         <Link className="w-full" href='/champions'>
-                            <a className="justify-center pt-16 font-sans text-5xl font-bold text-center text-black ">Champions</a>
+                            <a className="justify-center pt-16 font-sans text-5xl font-bold text-center ">Champions</a>
                         </Link>
                     </div>
 
-                    <div className="flex justify-center w-8/12 h-48 mt-24 rounded-lg bg-white/50 z-1">
+                    <div className="flex justify-center w-full h-48 mt-24 rounded-lg bg-white/50 z-1 text-black/50 hover:bg-white/75 hover:text-black/75 active:bg-white/25 active:text-black">
                         <Link className="w-full" href='/items'>
-                            <a className="justify-center pt-16 font-sans text-5xl font-bold text-center text-black ">Items</a>
+                            <a className="justify-center pt-16 font-sans text-5xl font-bold text-center ">Items</a>
                         </Link>
                     </div>
 
-                    <div className="flex justify-center w-8/12 h-48 mt-24 text-white rounded-lg shadow-2xl ml-44 bg-white/50 z-1">
+                    <div className="flex justify-center w-full h-48 mt-24 text-white rounded-lg shadow-2xl ml-44 bg-white/50 z-1 text-black/50 hover:bg-white/75 hover:text-black/75 active:bg-white/25 active:text-black ">
                         <Link className="w-full" href='#'>
-                            <a className="justify-center pt-16 font-sans text-5xl font-bold text-center text-black ">Item Browser</a>
+                            <a className="justify-center pt-16 font-sans text-5xl font-bold text-center ">Item Browser</a>
                         </Link>
                     </div>
 
-                    <div className="flex justify-center w-8/12 h-48 mt-24 rounded-lg shadow-2xl bg-white/50 z-1">
+                    <div className="flex justify-center w-full h-48 mt-24 rounded-lg shadow-2xl bg-white/50 z-1 text-black/50 hover:bg-white/75 hover:text-black/75 active:bg-white/25 active:text-black ">
                         <Link className="w-full" href='#'>
-                            <a className="justify-center pt-16 font-sans text-5xl font-bold text-center text-black">Champion Browser</a>
+                            <a className="justify-center pt-16 font-sans text-5xl font-bold text-center ">Champion Browser</a>
                         </Link>
                     </div>
 
                 </div>
-
             </div>
-        </>
+
+
+
+        </div>
     )
 }
